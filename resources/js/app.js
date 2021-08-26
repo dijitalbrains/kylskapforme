7e74d6b6068dfbee64d44d42271d4ec457fcc2f0
Ameer Hamza <ameerhamza12042@gmail.com>
2021-08-26 17:37:24 +0500

    swiper missing file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,15 @@
 // require('./bootstrap');
-import Alpine from 'alpinejs'
-window.Alpine = Alpine
-Alpine.start()
+import Alpine from "alpinejs";
+import Swiper from "swiper";
+import SwiperCore, { Pagination } from "swiper/core";
+
+window.Alpine = Alpine;
+Alpine.start();
+
+SwiperCore.use([Pagination]);
+new Swiper(".my-swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
