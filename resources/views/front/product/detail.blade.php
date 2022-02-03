0075b39f118e5515e2553871e81c377871925d11
Arshman <m.arshmanali196@gmail.com>
2022-02-03 17:17:29 +0500

    hide icon if not logged in

--- resources/views/front/product/detail.blade.php
+++ resources/views/front/product/detail.blade.php
@@ -18,10 +18,12 @@
             </div>
 
             <div class="flex justify-end space-x-2 my-4">
-                <a href="javascript:void(0)" @click="toggleFavorite({{ $refrigerator->id }}, $el)">
-                    <x-svg-heart-outline class="heart-outline {{ $refrigerator->isFavorite ? 'favourite' : '' }}">
-                    </x-svg-heart-outline>
-                </a>
+                @auth
+                    <a href="javascript:void(0)" @click="toggleFavorite({{ $refrigerator->id }}, $el)">
+                        <x-svg-heart-outline class="heart-outline {{ $refrigerator->isFavorite ? 'favourite' : '' }}">
+                        </x-svg-heart-outline>
+                    </a>
+                @endauth
                 <a href="javascript:void(0)">
                     <x-svg-share class="heart-outline"> </x-svg-share>
                 </a>
